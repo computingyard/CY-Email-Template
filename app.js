5d907e67868e09cbb7e85e54742546c59733cf80
MrFarhan <muhammadfarhan012345@gmail.com>
2021-07-01 17:07:15 +0500

    Created Thank you email template

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3002
+const nodemailer = require("nodemailer");
+const cors = require("cors")
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+
+app.use(cors())
+app.use(express.json());
+require('dotenv').config()
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.zoho.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass
+  }
+});
+
+
+app.post("/CYContactUs", async(req, res) => {
+  if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.message) {
+    return res.status(400).json({ success: false, message: "Missing required parameters" })
+  }
+  transporter.sendMail({
+    from: process.env.user, // sender address
+    to: process.env.user, // list of receivers
+    subject: `Message from ${req.body.firstName} ${req.body.lastName} at computing yard's website`, // Subject line
+    html: ` ${req.body.message} <br/>
+            `, // html body
+  });
+  transporter.sendMail({
+    from: process.env.user, // sender address
+    to: `${req.body.email}`,
+    subject: `Thank you ${req.body.firstName} ${req.body.lastName} `, // Subject line
+    html: await readFile('./EmailTemplete.html'), // html body
+  }).then(() => {
+    console.log("rq", req.body);
+    res.status(200).json({
+      success: true,
+      message: "Email successfully sent"
+    })
+  }).catch((err) => {
+    console.log("err", err)
+    res.status(400).json({
+      success: true,
+      message: err.message
+    })
+  })
+
+})
+
+
+app.listen(port, () => {
+  console.log(`CY Server listening at http://localhost:${port}`)
+})
+
+module.exports = app;
